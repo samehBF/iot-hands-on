2325259b64eebd83590f2cf5a0d70b738a58ea77
Qian Jin <qjin@xebia.fr>
2016-01-28 00:22:42 +0100

    add mqtt connection

--- embedded/temp_hum.py
+++ embedded/temp_hum.py
@@ -1,42 +1,46 @@
 #!/usr/bin/python
 
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import Adafruit_DHT
+import paho.mqtt.client as mqtt
 
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.DHT22
 
-# Example using a Beaglebone Black with DHT sensor
-# connected to pin P8_11.
-# pin = 'P8_11'
-
 # Example using a Raspberry Pi with DHT sensor
 # connected to GPIO23.
-pin = 23
+sensor_pin = 23
+
+# mqtt client event callbacks
+def on_connect(mosq, obj, rc):
+	print("rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+	print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(mosq, obj, mid):
+	print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+	print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mosq, obj, level, string):
+	print(string)
+
+# mqtt set up
+mqtt_broker_adress = "m20.cloudmqtt.com"
+mqtt_broket_port = 16301
+
+mqttClient = mqtt.Client()
+mqttClient_username = "jxhnfzti"
+mqttClient_password = "feCowLGfSWA7"
+
+mqttClient.username_pw_set(mqttClient_username, mqttClient_password)
+mqttClient.connect(mqtt_broker_adress, port=mqtt_broket_port, keepalive=60, bind_address="")
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
 
 # Note that sometimes you won't get a reading and
 # the results will be null (because Linux can't
@@ -44,5 +48,9 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # If this happens try again!
 if humidity is not None and temperature is not None:
 	print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+	data_buffer = json.dumps({"temp": temperature, "hum": humidity})
+		# publish data to broker
+		if mqttClient.publish("sensor", data_buffer):
+			print "data published to broker: " + str(data_buffer)
 else:
 	print 'Failed to get reading. Try again!'
