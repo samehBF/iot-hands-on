8d13911e4e73c340046f874db9464d42e2ccdffb
Qian Jin <qjin@xebia.fr>
2016-01-31 13:24:52 +0100

    Update data formatting & topic name

--- embedded/temp_hum.py
+++ embedded/temp_hum.py
@@ -48,10 +48,13 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
 # guarantee the timing of calls to read the sensor).  
 # If this happens try again!
 if humidity is not None and temperature is not None:
-	print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
-	data_buffer = json.dumps({"temp": temperature, "hum": humidity})
+	temp_fmt = '{0:0.1f}'.format(temperature)
+	hum_fmt = '{0:0.1f}'.format(humidity)
+	print 'Temp=%s*C  Humidity=%s%%' % (temperature, humidity)
+	
+	data_buffer = json.dumps({"temperature": temp_fmt, "humidity": hum_fmt})
 	# publish data to broker
-	if mqttClient.publish("sensor", data_buffer):
+	if mqttClient.publish("rasp_sensor", data_buffer):
 		print "data published to broker: " + str(data_buffer)
 else:
 	print 'Failed to get reading. Try again!'
