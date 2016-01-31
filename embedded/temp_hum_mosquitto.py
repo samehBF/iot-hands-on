974dd99531494b7557288598feab93b8e9a3e3a4
Qian Jin <qjin@xebia.fr>
2016-01-31 14:01:52 +0000

    fix indent

--- embedded/temp_hum_mosquitto.py
+++ embedded/temp_hum_mosquitto.py
@@ -24,27 +24,26 @@ rc = 0
 while rc == 0:
     rc = mqttc.loop()
     # Try to grab a sensor reading. Use the read_retry method which will retry up
-	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-	humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
 
-	# Note that sometimes you won't get a reading and
-	# the results will be null (because Linux can't
-	# guarantee the timing of calls to read the sensor).  
-	# If this happens try again!
-	if humidity is not None and temperature is not None:
-		temp_fmt = round(temperature, 1)
-		hum_fmt = round(humidity, 1)
-		print 'Temp=%s*C  Humidity=%s%%' % (temp_fmt, hum_fmt)
-		
-		# publish data to broker
-		data = json.dumps({"sensor_id": sensor_id, "temperature": temp_fmt, "humidity": hum_fmt})
-		if mqttc.publish("rasp_sensor", data):
-			print "Data published to broker: " + str(data)
-		else:
-			print "Failed to publish data to broker"
+    # Note that sometimes you won't get a reading and
+    # the results will be null (because Linux can't
+    # guarantee the timing of calls to read the sensor).  
+    # If this happens try again!
+    if humidity is not None and temperature is not None:
+	temp_fmt = round(temperature, 1)
+	hum_fmt = round(humidity, 1)
+	print 'Temp=%s*C  Humidity=%s%%' % (temp_fmt, hum_fmt)		
+	# publish data to broker
+	data = json.dumps({"sensor_id": sensor_id, "temperature": temp_fmt, "humidity": hum_fmt})
+	if mqttc.publish("rasp_sensor", data):
+		print "Data published to broker: " + str(data)
 	else:
-		print 'Failed to get reading. Try again!'
-	time.sleep(30)
+		print "Failed to publish data to broker"
+    else:
+	print 'Failed to get reading. Try again!'
+    time.sleep(30)
 
 print("rc: " + str(rc))
 
