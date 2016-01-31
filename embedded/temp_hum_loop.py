34d5077bcfa11e3231ddd3866952cc4e984d5cad
samehbf <sameh.benfredj@docapost-bpo.com>
2016-01-31 13:22:02 +0100

    add loop to read data and publish it every 10s

--- embedded/temp_hum_loop.py
+++ embedded/temp_hum_loop.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import Adafruit_DHT
+import time
 import paho.mqtt.client as mqtt
 import json
 
@@ -29,27 +30,38 @@ def on_log(mosq, obj, level, string):
 	print(string)
 
 # mqtt set up
-mqtt_broker_adress = ""
+mqtt_broker_adress = "m20.cloudmqtt.com"
 mqtt_broket_port = 16301
 
 mqttClient = mqtt.Client()
-mqttClient_username = ""
-mqttClient_password = ""
+mqttClient_username = "ksybowaf"
+mqttClient_password = "i9npMQNy1Xhd"
 
 mqttClient.username_pw_set(mqttClient_username, mqttClient_password)
 mqttClient.connect(mqtt_broker_adress, port=mqtt_broket_port, keepalive=60, bind_address="")
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
+#Loop to read data from sensor every 10 secondes
+cpt = 0
+while True:
+	cpt = cpt +1
+	time.sleep(10)
 
-# TODO: add Arduino-like loop
 
-if humidity is not None and temperature is not None:
-	print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
-	data_buffer = json.dumps({"temp": temperature, "hum": humidity})
-	# publish data to broker
-	if mqttClient.publish("sensor", data_buffer):
-		print "data published to broker: " + str(data_buffer)
-else:
-	print 'Failed to get reading. Try again!'
+	# Try to grab a sensor reading.  Use the read_retry method which will retry up
+	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
+
+	# TODO: add Arduino-like loop
+
+	if humidity is not None and temperature is not None:
+		print '------------------------'+ str(cpt) +'---------------------------'
+		print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+		data_buffer = json.dumps({"temp": temperature, "hum": humidity})
+		# publish data to broker
+		if mqttClient.publish("sensor", data_buffer):
+			print "data published to broker: " + str(data_buffer)
+			print '----------------------------------------------------'
+
+	else:
+		print 'Failed to get reading. Try again!'
+
